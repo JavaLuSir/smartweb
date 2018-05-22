ffc01a43809264048bdebbd8616de92b7bbc47db
javalusir <javalusir@163.com>
2018-05-22 22:34:09 +0800

    add hello jsp page

--- src/main/java/com/luxinx/demo/HelloServlet.java
+++ src/main/java/com/luxinx/demo/HelloServlet.java
@@ -14,7 +14,7 @@ import java.util.Date;
 public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("hello heaosd");
+
         Calendar calendar = Calendar.getInstance();
         Date time = calendar.getTime();
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
