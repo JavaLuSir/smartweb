3f7f3697861895307251d1f5cff698c23702689a
javalusir <javalusir@163.com>
2018-05-28 23:27:22 +0800

    add basic model service page dependency jars and so on

---
+++ src/main/java/com/luxinx/test/CustomerService.java
@@ -0,0 +1,29 @@
+package com.luxinx.test;
+
+
+import com.luxinx.model.Customer;
+
+import java.util.List;
+import java.util.Map;
+
+public class CustomerService {
+
+    public List<Customer> getCustomerList(String keyword){
+        return null;
+    }
+
+    public Customer getCustomer(long id){
+        return null;
+    }
+
+    public boolean createCustomer(Map<String, Object> fieldMap){
+        return false;
+    }
+
+    public boolean updateCustomer(long id , Map<String, Object> fieldMap){
+        return false;
+    }
+    public boolean deleteCustomer(long id){
+        return false;
+    }
+}
---
+++ src/main/java/com/luxinx/util/PropsUtil.java
@@ -0,0 +1,11 @@
+package com.luxinx.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class PropsUtil {
+    private static final Logger log = LoggerFactory.getLogger(PropsUtil.class);
+
+
+}
